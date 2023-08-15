6ab2c32f9baa532876e93d083918e02a22116ac5
Mayur Kachhadiya <mayurkachhadiya373@gmail.com>
2023-08-15 18:31:44 +0530

    registration page

--- App.js
+++ App.js
@@ -1,10 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import LoginScreen from './app/Screens/LoginScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import 'react-native-gesture-handler';
+import { StyleSheet, Text, View,LogBox } from 'react-native';
+import AuthContext from './app/auth/context';
+import OfflineNotice from './app/components/offlineNotice';
+import AuthNavigator from './app/Navigator/AuthNavigator';
+import MainNavigator from './app/Navigator/MainNavigator';
+import AppLoading from 'expo-app-loading';
+import React,{useState} from 'react';
+import authStore from './app/auth/storage';
+import Firebase from './app/config/firebase';
 
 export default function App() {
+  const db = Firebase.firestore();
+  const [userData,setUserData] = useState(null);
+  const [isReady,setReady] = useState(false);
+
+  const restoreData = async () => {
+    const user = await authStore.getData();
+    if(user){
+      const {uid} = JSON.parse(user);
+      await db.collection('hospitals').doc(uid).get()
+      .then(snapshot => 
+        setUserData(snapshot.data())
+        );
+    }
+  }
+
+  if(!isReady){
+    return <AppLoading onError={console.warn} startAsync={restoreUser} onFinish={() => setReady(true)} />
+  }
+
+
   return (
-    <LoginScreen/>
+    <AuthContext.Provider value={{userData,setUserData}}>
+     <OfflineNotice/>
+     <NavigationContainer>
+      {userData ? <MainNavigator /> : <AuthNavigator />}
+     </NavigationContainer>
+   </AuthContext.Provider>
   );
 }
 
